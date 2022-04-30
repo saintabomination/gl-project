b9814c5b1bfab2f856770257d459e85ba37855eb
saintabomination <saint.abomination@protonmail.com>
2022-04-30 12:36:47 +0200

    Creating a GLFW window

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,6 +2,9 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+const unsigned int WINDOW_WIDTH = 800;
+const unsigned int WINDOW_HEIGHT = 600;
+
 int main()
 {
   if (!glfwInit())
@@ -10,11 +13,28 @@ int main()
     return -1;
   }
 
+  // Window Hints
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* window;
+  window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "GLFW", NULL, NULL);
+  if (window == NULL)
+  {
+    std::cout << "Failed to initialise GLFW Window!\n";
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
   GLenum err = glewInit();
   if (err != GLEW_OK)
   {
     std::cout << "Failed to initialise GLEW!\n";
     std::cout << "Error: " << glewGetErrorString(err) << '\n';
+    return -1;
   }
 
   glfwTerminate();
