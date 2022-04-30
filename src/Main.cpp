18c5425bfc48f904f1235adc461b1bc906e0a285
saintabomination <saint.abomination@protonmail.com>
2022-04-30 20:57:13 +0200

    Creating shaders

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,8 +5,26 @@
 const unsigned int WINDOW_WIDTH = 800;
 const unsigned int WINDOW_HEIGHT = 600;
 
+const char* vertexShaderSource =
+  "#version 330 core\n"
+  "layout (location = 0) in vec3 aPos;\n"
+  "void main()\n"
+  "{\n"
+  " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.f);\n"
+  "}\0";
+
+const char* fragmentShaderSource =
+  "#version 330 core\n"
+  "out vec4 FragColor;\n"
+  "void main()\n"
+  "{\n"
+  " FragColor = vec4(1.f, 0.f, 0.f, 1.f);\n"
+  "}\0";
+
 int main()
 {
+  // GLFW Intialisation
+
   if (!glfwInit())
   {
     std::cout << "Failed to initialise GLFW!\n";
@@ -14,6 +32,7 @@ int main()
   }
 
   // Window Hints
+
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
@@ -29,6 +48,8 @@ int main()
   }
   glfwMakeContextCurrent(window);
 
+  // GLEW Intialisation
+
   GLenum err = glewInit();
   if (err != GLEW_OK)
   {
@@ -43,10 +64,56 @@ int main()
     0.f, 1.f, 0.f
   };
 
+  // VBO
+
   GLuint vertexBuffer;
   glGenBuffers(1, &vertexBuffer);
   glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  
+  // Creating Shaders
+
+  GLuint vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  int shaderSuccess;
+  char infoLog[512];
+
+  // GLSL Error Checking
+
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &shaderSuccess);
+  if (!shaderSuccess)
+  {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cout << "Shader compilation failed!\n" << infoLog << '\n';
+  }
+
+  GLuint fragmentShader;
+  vertexShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &shaderSuccess);
+  if (!shaderSuccess)
+  {
+    glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+    std::cout << "Shader compilation failed!\n" << infoLog << '\n';
+  }
+
+  // Linking Shaders to Shader Program
+
+  unsigned int shaderProgram;
+  shaderProgram = glCreateProgram();
+
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+  glUseProgram(shaderProgram);
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
 
   while (!glfwWindowShouldClose(window))
   {
