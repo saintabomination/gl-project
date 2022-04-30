6083884c7f12768fc018a0d1c87c96e4b1f102d8
saintabomination <saint.abomination@protonmail.com>
2022-04-30 13:54:07 +0200

    VBO Base

--- src/Main.cpp
+++ src/Main.cpp
@@ -37,6 +37,37 @@ int main()
     return -1;
   }
 
+  float vertices[] = {
+    -1.f, -1.f, 0.f,
+    1.f, -1.f, 0.f,
+    0.f, 1.f, 0.f
+  };
+
+  GLuint vertexBuffer;
+  glGenBuffers(1, &vertexBuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glVertexAttribPointer(
+      0,
+      3,
+      GL_FLOAT,
+      GL_FALSE,
+      0,
+      (void*)0
+    );
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDisableVertexAttribArray(0);
+  }
+
   glfwTerminate();
   return 0;
 }
